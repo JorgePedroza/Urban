a4029e0db986938918501f72eeb0c444c39c00f4
JorgePedroza <61950919+JorgePedroza@users.noreply.github.com>
2020-05-25 22:59:14 -0500

    dasd

--- lib/pages/terminos_page.dart
+++ lib/pages/terminos_page.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_plugin_pdf_viewer/flutter_plugin_pdf_viewer.dart';
 
-class TerminosPage extends StatelessWidget {
+class TerminosPage extends StatefulWidget {
   
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-appBar: AppBar(
-  title: Text('Terminos y condiciones', style: TextStyle(fontSize: 26),),
-),
+  _TerminosPageState createState() => _TerminosPageState();
+}
 
+class _TerminosPageState extends State<TerminosPage> {
+  
+  bool _isLoading = true;
+  PDFDocument document;
+
+  @override
+  void initState() {
+    super.initState();
+    loadDocument();
+  }
+
+  loadDocument() async {
+    document = await PDFDocument.fromAsset('assets/sample.pdf');
+    setState(() => _isLoading = false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+            debugShowCheckedModeBanner: false,
+          home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Terminos y condiciones'),
+          ),
+          body: Center(
+              child: _isLoading
+                  ? Center(child: CircularProgressIndicator())
+                  : PDFViewer(document: document, showPicker: false)),
+        
+      ),
     );
   }
 }
